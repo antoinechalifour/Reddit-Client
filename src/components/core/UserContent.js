e463665e4ad4bb0a09ec7faf9d46c6e9edd4d3ee
antoinechalifour <antoine.chalifour@gmail.com>
2018-03-26 15:20:23 +0200

    fix: Import only common languages

--- src/components/core/UserContent.js
+++ src/components/core/UserContent.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import highlight from 'highlight.js'
+import highlight from 'highlight.js/lib/highlight'
 import Markdown from 'react-remarkable'
 import decodeHtmlEntities from 'decode-html'
+import javascript from 'highlight.js/lib/languages/javascript'
+import css from 'highlight.js/lib/languages/css'
+import java from 'highlight.js/lib/languages/java'
+import xml from 'highlight.js/lib/languages/xml'
+import python from 'highlight.js/lib/languages/python'
+import php from 'highlight.js/lib/languages/php'
+import rust from 'highlight.js/lib/languages/rust'
+import typescript from 'highlight.js/lib/languages/typescript'
+import cs from 'highlight.js/lib/languages/cs'
+import cpp from 'highlight.js/lib/languages/cpp'
+
+highlight.registerLanguage('javascript', javascript)
+highlight.registerLanguage('css', css)
+highlight.registerLanguage('java', java)
+highlight.registerLanguage('xml', xml)
+highlight.registerLanguage('python', python)
+highlight.registerLanguage('php', php)
+highlight.registerLanguage('rust', rust)
+highlight.registerLanguage('typescript', typescript)
+highlight.registerLanguage('cs', cs)
+highlight.registerLanguage('cpp', cpp)
 
 export default class UserContent extends Component {
   static propTypes = {
